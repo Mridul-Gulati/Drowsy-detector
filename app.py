8ba02aa4b8cfcf01866b1f1c6364b1e18fa063c2
Mridul Gulati <90506788+Mridul-Gulati@users.noreply.github.com>
2024-01-25 02:22:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def gen_frames():
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed', methods=['POST'])
+@app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
